a684b4031413e1eff8f08a9910d3e294bcb43660
Iván García <66884918+IvanGH4@users.noreply.github.com>
2021-05-13 13:04:32 -0300

    fix routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,26 +3,29 @@ const router = express.Router();
 const registerController = require("../controllers/registerController");
 const loginController = require("../controllers/loginController");
 const socialController = require("../controllers/socialController");
-//const checkToken = require("../middlewares/checkToken");
-const checkToken = require("express-jwt");
-const checkAuthor = require("../middlewares/checkAuthor");
 const homeController = require("../controllers/homeController");
 const profileController = require("../controllers/profileController");
+const checkToken = require("express-jwt");
+const checkAuthor = require("../middlewares/checkAuthor");
 
-router.post("/register", registerController.store);
-router.post("/login", loginController.login);
+router.post("/users", registerController.store);
+router.post("/tokens", loginController.login);
+
 router.use(
   checkToken({ secret: process.env.SECRET_TEXT, algorithms: ["HS256"] })
 );
 
-router.get("/index-tweets", homeController.indexTweets);
-router.get("/index-users", homeController.indexUsers);
+router.get("/tweets", homeController.indexTweets);
+router.get("/users", homeController.indexUsers);
 router.get("/index", homeController.index);
-router.get("/index-user", profileController.show); // a los get no se les puede pasar body, con axios se le setea la info por params y se recibe en query.
-router.post("/create-tweet", socialController.store);
-router.delete("/delete-tweet", [checkAuthor], socialController.destroy);
-router.patch("/follow", socialController.follow);
-router.post("/like", socialController.like);
-router.patch("/update-profile", profileController.update);
+router.get("/users/profile", profileController.show); // a los get no se les puede pasar body, con axios se le setea la info por params y se recibe en query.
+
+router.post("/tweets", socialController.store);
+
+router.delete("/tweets", [checkAuthor], socialController.destroy);
+
+router.patch("/users", socialController.follow);
+router.patch("/tweets", socialController.like);
+router.patch("/users/profile", profileController.update);
 
 module.exports = router;
